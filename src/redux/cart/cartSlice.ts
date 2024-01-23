b6fb0e8bbb37ed7b0f510fa7ac204450acc60bde
unknown <Shikder-Vai>
2024-01-24 00:22:53 +0600

    first commit

---
+++ src/redux/cart/cartSlice.ts
@@ -0,0 +1,37 @@
+import { IProduct } from '@/types/globalTypes';
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+
+interface ICart {
+  products: IProduct[];
+}
+
+const initialState: ICart = {
+  products: [],
+};
+
+const cartSlice = createSlice({
+  name: 'Cart',
+  initialState,
+  reducers: {
+    addToCart: (state, action: PayloadAction<IProduct>) => {
+      const existing = state.products.find(
+        (product) => product._id === action.payload._id
+      );
+
+      if (existing) {
+        existing.quantity = existing.quantity! + 1;
+      } else {
+        state.products.push({ ...action.payload, quantity: 1 });
+      }
+    },
+    removeFromCart: (state, action: PayloadAction<IProduct>) => {
+      state.products = state.products.filter(
+        (product) => product._id !== action.payload._id
+      );
+    },
+  },
+});
+
+export const { addToCart, removeFromCart } = cartSlice.actions;
+export default cartSlice.reducer;
