3f9cee7a950da3111975798f9b0a50d1f4709672
unknown <Shikder-Vai>
2024-01-26 02:04:41 +0600

    user authentication half done

--- src/redux/Features/user/userSlice.ts
+++ src/redux/Features/user/userSlice.ts
@@ -1,4 +1,6 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { auth } from '@/lib/firebase';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 // import type { PayloadAction } from '@reduxjs/toolkit';
 
 interface IUserState {
@@ -10,6 +12,11 @@ interface IUserState {
   Error: string | null;
 }
 
+interface ICredintial {
+  email: string;
+  password: string;
+}
+
 const initialState: IUserState = {
   user: {
     email: null,
@@ -19,10 +26,27 @@ const initialState: IUserState = {
   Error: null,
 };
 
+export const createUser = createAsyncThunk(
+  'user/createUser',
+  async ({ email, password }: ICredintial) => {
+    const data = await createUserWithEmailAndPassword(auth, email, password);
+
+    return data?.user?.email;
+  }
+);
+
 const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createUser.fulfilled, () => {})
+      .addCase(createUser.rejected, () => {});
+  },
 });
 
 export const userReducer = userSlice.reducer;
